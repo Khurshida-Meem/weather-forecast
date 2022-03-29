52680383c8dc847244012bb12a8da3c152da3bc8
Khurshida Jahan Meem <meemrumi59@gmail.com>
2022-03-29 13:49:59 +0600

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast= new Forecast();
+
+const updateUI = data => {
+
+    const {cityDets,weather} = data;
+
+    // update details 
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+        </div>  
+    `;
+
+    // update day/night and icons
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc)
+
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+    // remove d-none class if exist
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+}
+
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    forecast.updateCity(city).then(data => updateUI(data));
+
+    // set local storage
+    localStorage.setItem('city', city);
+})
+
+if(localStorage.getItem('city')){
+    forecast.updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err))
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,63 @@
+class Forecast {
+    constructor() {
+        this.key = 'oyAI23u14xroCH7O7xAxqGETUqabfAb1';
+        this.weatherUri = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityUri = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+
+        // object shorthand notation
+        return { cityDets, weather };
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityUri + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+    async getWeather(id) {
+
+        const query = `${id}?apikey=${this.key}`;
+
+        const response = await fetch(this.weatherUri + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+}
+
+
+// ========================== functional way ======================
+
+// const key = 'oyAI23u14xroCH7O7xAxqGETUqabfAb1';
+
+// // get weather info
+
+// const getWeather = async (id) => {
+//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     const query = `${id}?apikey=${key}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+
+// }
+
+// //  get city info
+// const getCity = async (city) => {
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+//     const query = `?apikey=${key}&q=${city}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+// };
+
+
+
+
